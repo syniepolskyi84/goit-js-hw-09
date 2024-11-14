078ad7fc65790667ecdab4ec0a20d9f1a4296c11
syniepolskyi84 <e.syniepolskyi@gmail.com>
2024-11-14 18:40:49 +0200

    add 1-gallery, 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,61 @@
-console.log('Form');
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (email === "" || message === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const data = {
+      email,
+      message,
+    };
+    console.log(data);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  }
+}
+
+function handleInput() {
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  const data = {
+    email,
+    message,
+  };
+
+  saveToLS(STORAGE_KEY, data);
+}
+
+function saveToLS(key, value) {
+  const zip = JSON.stringify(value);
+  localStorage.setItem(key, zip);
+}
+
+function loadFromLS(key) {
+  const zip = localStorage.getItem(key);
+  try {
+    return JSON.parse(zip);
+  } catch {
+    return zip;
+  }
+}
+
+function init() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+
+init();
